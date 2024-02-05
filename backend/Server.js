3fd16bb4a5a4e8bb045017a6d945cf4c04507f42
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-02-06 00:01:22 +0530

    Routes Setup

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,8 @@
 const app= require("express")();
 const dotenv= require("dotenv");
 const ConnectDB = require("./config/ConnectDB");
+const userRoutes = require("./routes/user/userRoutes")
+const adminRoutes= require('./routes/admin/adminRoutes')
 
 dotenv.config();
 
@@ -11,3 +13,7 @@ const port= process.env.PORT || 4000
 app.listen(port,async ()=>{
     console.log(`Listening on port ${port}!`);
 })
+
+app.use('/route/user/',userRoutes);
+
+app.use('/route/admin/',adminRoutes);
